6b6a1b707951bb9ddefae1a5adf96cf05a215b19
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-01 12:22:56 +0000

    17 ran tests, no prediction, got amber

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -25,7 +25,7 @@ public class MoneyTest {
     @Test
     void testSimpleAddition() {
         Money five = Money.dollar(5);
-        Expression sum = five(5).plus(five);
+        Expression sum = five.plus(five);
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
