622968efc18f8244eab4e39069cac4651599ee9d
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-01 12:09:54 +0000

    8 ran tests, no prediction, got green

--- money/MoneyTest.java
+++ money/MoneyTest.java
@@ -9,8 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MoneyTest {
 
     @Test
+    void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    }
+
+    
+    @Test
     void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(5), Money.dollar(5));
     }
+    
 }
