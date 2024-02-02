f5f1e94f9d8b1eded84a3fc9bd058bb05598371a
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-02 12:56:51 +0000

    81 ran tests, no prediction, got green

--- money/Money.java
+++ money/Money.java
@@ -1,7 +1,7 @@
 interface Expression {
     Money reduce(Bank bank, String to);
     Expression plus(Expression addend);
-    Expression times(Expression multiplier);
+    Expression times(int multiplier);
 }
 
 public class Money implements Expression {
--- money/Sum.java
+++ money/Sum.java
@@ -16,7 +16,7 @@ class Sum implements Expression {
         return new Sum(this, addend);
     }
     
-    public Expression times(Expression multiplier) {
+    public Expression times(int multiplier) {
         return new Sum(augend.times(multiplier), addend.times(multiplier));
     }
 
