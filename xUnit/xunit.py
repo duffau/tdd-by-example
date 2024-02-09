7f6a7c626808fb1410b2c697c8ae0941146d9d32
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-09 21:44:57 +0100

    xUnit: green - add method call log in WsaRun

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -18,6 +18,7 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self):
         self.wasRun = 1
