4f8c8c9353b132635199a07b5d644dabe3fb7cce
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-09 22:15:22 +0100

    xUnit: green - increment runCount in TestResult and use result object in TestCase.run

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -9,17 +9,22 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
-        return TestResult()
+        return result
 
 
 class TestResult:
 
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount += 1
 
     def summary(self):
         return f"{self.runCount} run, 0 failed"
