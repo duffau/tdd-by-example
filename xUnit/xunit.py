3321c8b4ce14f35d21d19af87ada1b9dc9a1a602
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-09 22:12:03 +0100

    xUnit: green - add runCount as a symbolic constant  in TestResult

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -18,8 +18,11 @@ class TestCase:
 
 class TestResult:
 
+    def __init__(self):
+        self.runCount = 1
+
     def summary(self):
-        return "1 run, 0 failed"
+        return f"{self.runCount} run, 0 failed"
 
 
 class WasRun(TestCase):
