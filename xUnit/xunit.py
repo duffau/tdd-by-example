632f9f04f0ee93a8270502ff6dd1ed552dfecb92
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-09 14:05:54 +0100

    xUnit: passing test - move name attribute to TesCase superclass

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -1,10 +1,11 @@
 class TestCase:
-    pass
+    def __init__(self, name):
+        self.name = name
 
 class WasRun(TestCase):
     def __init__(self, name):
+        super().__init__(name)
         self.wasRun = None
-        self.name = name
     
     def testMethod(self):
         self.wasRun = 1
