2b7187698eb527131b8d39cdd4235fe6a8b8e6e9
vCG9DZ <vCG9DZ@cyber-dojo.org>
2024-02-09 14:08:21 +0100

    xUnit: passing test - move run method to TesCase superclass

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -2,14 +2,15 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
 class WasRun(TestCase):
     def __init__(self, name):
         super().__init__(name)
         self.wasRun = None
-    
+
     def testMethod(self):
         self.wasRun = 1
-
-    def run(self):
-        method = getattr(self, self.name)
-        method()
